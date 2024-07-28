b1d292a0c6f605c3a1c0a0ca10ddc1b76b27a077
Sergiy Zhurenko <sergeyszhurenko@gmail.com>
2024-07-29 02:05:40 +0300

    all functions works, but need to fix excel+pages

--- Apartment.py
+++ Apartment.py
@@ -1,12 +1,32 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
+from time import sleep
 
-url = 'https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text,'lxml')
-name = soup.find('div',class_='css-1sw7q4x').find('h6',class_='css-1wxaaza').text
-price = soup.find('div', class_='css-1sw7q4x').find('p', class_='css-13afqrm').text
-link = 'https://www.olx.ua'+soup.find('div', class_='css-1sw7q4x').find('a', class_='css-z3gu2d').get('href')
-print(name)
-print(price)
-print(link)
+data = []
+
+for i in range(1,26):
+    print(i)
+    url = 'https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/'
+    response = requests.get(url)
+    sleep(3)
+    soup = BeautifulSoup(response.text,'lxml')
+    Apartments = soup.findAll('div', class_='css-1sw7q4x')
+
+for Apartment in Apartments:
+
+    try:
+        name = Apartment.find('h6', class_='css-1wxaaza').text
+        price = Apartment.find('p', class_='css-13afqrm').text
+        link = 'https://www.olx.ua' + Apartment.find('a', class_='css-z3gu2d').get('href')
+    except:
+        name = '--'
+        price = '--'
+        link = '--'
+    data.append([name, price, link])
+
+
+print(data)
+header = [['Адреса', 'Ціна', 'Посилання']]
+df = pd.DataFrame(data, columns=header)
+df.to_csv('/Users/Gyrocopter_UA/Desktop/Apartment.csv', sep =';', encoding ='ansi')
