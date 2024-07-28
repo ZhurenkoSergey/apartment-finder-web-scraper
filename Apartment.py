d482493913f3f22ebf88d6808adb8f418cfe001d
Sergiy Zhurenko <sergeyszhurenko@gmail.com>
2024-07-29 00:53:47 +0300

    added name and price parsing

--- Apartment.py
+++ Apartment.py
@@ -4,5 +4,9 @@ from bs4 import BeautifulSoup
 url = 'https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text,'lxml')
+name = soup.find('div',class_='css-1sw7q4x').find('h6',class_='css-1wxaaza').text
+price = soup.find('div', class_='css-1sw7q4x').find('p', class_='css-13afqrm').text
 link = 'https://www.olx.ua'+soup.find('div', class_='css-1sw7q4x').find('a', class_='css-z3gu2d').get('href')
+print(name)
+print(price)
 print(link)
