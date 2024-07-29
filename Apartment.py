4cc0f624c6dea01fe15d306c3dfd443e99a3f344
Sergiy Zhurenko <sergeyszhurenko@gmail.com>
2024-07-29 14:02:26 +0300

    project is working, fixed pages and excel utf8

--- Apartment.py
+++ Apartment.py
@@ -7,26 +7,25 @@ data = []
 
 for i in range(1,26):
     print(i)
-    url = 'https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/'
+    url = f'https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/?page={i}'
     response = requests.get(url)
     sleep(3)
     soup = BeautifulSoup(response.text,'lxml')
     Apartments = soup.findAll('div', class_='css-1sw7q4x')
 
-for Apartment in Apartments:
+    for Apartment in Apartments:
+        try:
+            name = Apartment.find('h6', class_='css-1wxaaza').text
+            price = Apartment.find('p', class_='css-13afqrm').text
+            link = 'https://www.olx.ua' + Apartment.find('a', class_='css-z3gu2d').get('href')
+        except:
+            name = '-'
+            price = '-'
+            link = '-'
 
-    try:
-        name = Apartment.find('h6', class_='css-1wxaaza').text
-        price = Apartment.find('p', class_='css-13afqrm').text
-        link = 'https://www.olx.ua' + Apartment.find('a', class_='css-z3gu2d').get('href')
-    except:
-        name = '--'
-        price = '--'
-        link = '--'
-    data.append([name, price, link])
+        data.append([name, price, link])
 
 
-print(data)
 header = [['Адреса', 'Ціна', 'Посилання']]
 df = pd.DataFrame(data, columns=header)
-df.to_csv('/Users/Gyrocopter_UA/Desktop/Apartment.csv', sep =';', encoding ='ansi')
+df.to_csv('/Users/Gyrocopter_UA/Desktop/Apartment.csv', sep =';', encoding ='utf8')
